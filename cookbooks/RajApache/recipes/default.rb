76806e4acbdb985055f2870138b4f31d968232d3
raj <x>
2013-10-31 16:28:19 -0700

    After the Virtual Hosts Example Finished

--- cookbooks/RajApache/recipes/default.rb
+++ cookbooks/RajApache/recipes/default.rb
@@ -7,23 +7,51 @@
 # All rights reserved - Do Not Redistribute
 #
 package "apache2" do
-	action :install
+  action :install
 end
-
-execute "a2dissite default" do
-	only_if do
-		File.symlink?(:wq
-
-	end
-end
-
-
-
+ 
 service "apache2" do
-	action [ :enable, :start ]
+  action [ :start, :enable ]
 end
-
-cookbook_file "/var/www/index.html" do
-	source "index.html"
-	mode "0644"
+ 
+execute "a2dissite default" do
+  only_if do
+    File.symlink?("/etc/apache2/sites-enabled/000-default")
+  end
+  notifies :restart, "service[apache2]"
+end
+ 
+node['apache']['sites'].each do |site_name, site_data|
+  document_root = "/srv/apache/#{site_name}"
+ 
+  template "/etc/apache2/sites-available/#{site_name}" do
+    source "custom.erb"
+    mode "0644"
+    variables(
+      :document_root => document_root,
+      :port => site_data['port']
+    )
+    notifies :restart, "service[apache2]"
+  end
+ 
+  execute "a2ensite #{site_name}" do
+    not_if do
+      File.symlink?("/etc/apache2/sites-enabled/#{site_name}")
+    end
+    notifies :restart, "service[apache2]"
+  end
+ 
+  directory document_root do
+    mode "0755"
+    recursive true
+  end
+ 
+  template "#{document_root}/index.html" do
+    source "index.html.erb"
+    variables(
+      :site_name => site_name,
+      :port => site_data['port']
+    )
+    mode "0644"
+  end
 end
