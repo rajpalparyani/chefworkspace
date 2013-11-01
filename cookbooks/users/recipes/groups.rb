b1b259a31655f0b689ce79629ee20955c82d362d
raj <x>
2013-11-01 10:39:34 -0700

    After the Users Data Bag Example

---
+++ cookbooks/users/recipes/groups.rb
@@ -0,0 +1,16 @@
+#
+# Cookbook Name:: users
+# Recipe:: default
+#
+# Copyright 2013, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+search(:groups, "*:*").each do |group_data|
+	group group_data['id'] do
+		gid group_data['gid']
+		members group_data['members']
+	end
+end
+		
+		
